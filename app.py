4a63c18254bd68538dd0f3394c49d6b6a9dc61f0
rhmaadevii <rhmaadevi@gmail.com>
2026-07-05 11:40:50 +0700

    Use Firebase env variable

--- app.py
+++ app.py
@@ -16,7 +16,15 @@ CORS(app)
 # Firebase
 # ===========================
 
-cred = credentials.Certificate("serviceAccountKey.json")
+import os
+import json
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+firebase_key = json.loads(os.environ["FIREBASE_KEY"])
+
+cred = credentials.Certificate(firebase_key)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
